94316cb1bdcbc8ab996a1af0c28659e371f4e136
Quyên <49813373+QuyenTraan@users.noreply.github.com>
2025-01-22 11:09:02 +0700

    Lesson 1: input box, type, clear, slow typing
    Lesson 1: input box, type, clear, slow typing

---
+++ test/features/demo/WebInteraction.feature
@@ -0,0 +1,10 @@
+Feature: Web Interactionss
+
+Scenario Outline: Demo Web Interactions
+
+Given A web page is openned
+When Perform web interations
+
+Examples:
+    | TestId | 
+    | WEB_TC002 |
--- test/features/step-definitions/steps.js
+++ test/features/step-definitions/steps.js
@@ -1,5 +1,5 @@
 import { Given, When, Then } from '@wdio/cucumber-framework';
-import { expect, $ } from '@wdio/globals'
+import { expect, $, browser } from '@wdio/globals'
 
 import LoginPage from '../pageobjects/login.page.js';
 import SecurePage from '../pageobjects/secure.page.js';
@@ -20,4 +20,40 @@ Then(/^I should see a flash message saying (.*)$/, async (message) => {
     await expect(SecurePage.flashAlert).toBeExisting();
     await expect(SecurePage.flashAlert).toHaveText(expect.stringContaining(message));
 });
+/**
+ * Web interaction
+ */
+Given(/^A web page is openned$/, async function () {
+
+    await browser.url("https://the-internet.herokuapp.com/inputs")
+    await browser.setTimeout({ implicit: 15000, pageLoad: 10000 })
+    await browser.maximizeWindow()
+
+}
+
+)
+When(/^Perform web interations$/, async function () {
+    /**
+     * 1.Input box
+     * Action
+     * 1. Type into input box
+     * 2. Clear the field and type or just add value
+     * 3. Click and type
+     * 4. Slow typing
+     */
+
+    let num = 1234
+    let strNum = num.toString()
+    let ele = await $('[type=number]')
+    await ele.click()
+
+    for (let i = 0; i < strNum.length; i++) {
+        let charStr = strNum.charAt(i)
+       
+        await browser.keys(charStr)
+
+    }
+    
+
+})
 
