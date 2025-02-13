a3727f918fefa49acc8a9424c41028e7b5adf10b
Quyên <49813373+QuyenTraan@users.noreply.github.com>
2025-02-13 14:57:02 +0700

    update dropdown

--- test/features/step-definitions/steps.js
+++ test/features/step-definitions/steps.js
@@ -1,5 +1,7 @@
 import { Given, When, Then } from '@wdio/cucumber-framework';
 import { expect, $, browser } from '@wdio/globals'
+import * as chai from 'chai';
+
 
 import LoginPage from '../pageobjects/login.page.js';
 import SecurePage from '../pageobjects/secure.page.js';
@@ -9,23 +11,23 @@ const pages = {
 }
 
 Given(/^I am on the (\w+) page$/, async (page) => {
-    await pages[page].open()
+    // await pages[page].open()
 });
 
 When(/^I login with (\w+) and (.+)$/, async (username, password) => {
-    await LoginPage.login(username, password)
+    // await LoginPage.login(username, password)
 });
 
 Then(/^I should see a flash message saying (.*)$/, async (message) => {
-    await expect(SecurePage.flashAlert).toBeExisting();
-    await expect(SecurePage.flashAlert).toHaveText(expect.stringContaining(message));
+    // await expect(SecurePage.flashAlert).toBeExisting();
+    // await expect(SecurePage.flashAlert).toHaveText(expect.stringContaining(message));
 });
 /**
  * Web interaction
  */
 Given(/^A web page is openned$/, async function () {
 
-    await browser.url("https://the-internet.herokuapp.com/inputs")
+    await browser.url("https://the-internet.herokuapp.com/dropdown")
     await browser.setTimeout({ implicit: 15000, pageLoad: 10000 })
     await browser.maximizeWindow()
 
@@ -42,18 +44,55 @@ When(/^Perform web interations$/, async function () {
      * 4. Slow typing
      */
 
-    let num = 1234
-    let strNum = num.toString()
-    let ele = await $('[type=number]')
-    await ele.click()
+    // let num = 1234
+    // let strNum = num.toString()
+    // let ele = await $('[type=number]')
+    // await ele.click()
 
-    for (let i = 0; i < strNum.length; i++) {
-        let charStr = strNum.charAt(i)
-       
-        await browser.keys(charStr)
+    // for (let i = 0; i < strNum.length; i++) {
+    //     let charStr = strNum.charAt(i)
 
+    //     await browser.keys(charStr)
+
+    // }
+
+    /**
+     * 2.Dropdown
+     * Action
+     * 1. Assert default option is selected
+     * 2. Select by atribute, text, index
+     * 3. Get a list of option
+     */
+    //Assert default option is selected
+    // let ele1 = await $('//select/option[@selected="selected"]')
+    // let val = await ele1.getText()
+    // chai.expect(val).to.equal("Please select an option")
+
+    // //Select a specific option
+    // let ddEle = await $(`#dropdown`)
+
+    // await ddEle.selectByVisibleText("Option 2")
+
+    // await ddEle.selectByAttribute("value","1")
+    // await ddEle.selectByIndex(2)
+
+    /**
+     * 3. Get a list of option
+     */
+
+    let eleArr = await $$(`select>option`);
+    let arr = [];
+    for (let i = 0; i < eleArr.length; i++) { // Corrected the typo here
+        let ele = eleArr[i];
+        let val = await ele.getText();
+        arr.push(val);
+        console.log(val);
     }
-    
+
+    console.log(`>>Option array: ${arr}`);
+
+
+    await browser.debug()
 
 })
 
